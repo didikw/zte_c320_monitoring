31e65bc1c418bad3748062891387f8af01263409
didikw <30753698+didikw@users.noreply.github.com>
2021-06-07 10:50:02 +0700

    Update onustate.php

--- onustate.php
+++ onustate.php
@@ -18,12 +18,12 @@ echo $get_http_response_code;
 $connlok = new PDO('mysql:host=xxx;dbname=olt', 'yyy', 'zzz');
 $connlok->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-$lokasi = snmpwalk("10.1.2.2", "publicblitark4b", ".1.3.6.1.4.1.3902.1012.3.28.1.1.3"); 
-$status = snmpwalk("10.1.2.2", "publicblitark4b", "ZXGPON-SERVICE-MIB::zxGponOntPhaseState"); 
-$lastonline = snmpwalk("10.1.2.2", "publicblitark4b", "ZXGPON-SERVICE-MIB::zxGponOnuLastOnlineTime"); 
-$lastoffline = snmpwalk("10.1.2.2", "publicblitark4b", "ZXGPON-SERVICE-MIB::zxGponOnuLastOfflineTime");
-$onurx = snmpwalk("10.1.2.2", "publicblitark4b", ".1.3.6.1.4.1.3902.1012.3.50.12.1.1.10");
-$onutx = snmpwalk("10.1.2.2", "publicblitark4b", ".1.3.6.1.4.1.3902.1012.3.50.12.1.1.14");
+$lokasi = snmpwalk("OLT-IP", "community", ".1.3.6.1.4.1.3902.1012.3.28.1.1.3"); 
+$status = snmpwalk("OLT-IP", "community", "ZXGPON-SERVICE-MIB::zxGponOntPhaseState"); 
+$lastonline = snmpwalk("OLT-IP", "community", "ZXGPON-SERVICE-MIB::zxGponOnuLastOnlineTime"); 
+$lastoffline = snmpwalk("OLT-IP", "community", "ZXGPON-SERVICE-MIB::zxGponOnuLastOfflineTime");
+$onurx = snmpwalk("OLT-IP", "community", ".1.3.6.1.4.1.3902.1012.3.50.12.1.1.10");
+$onutx = snmpwalk("OLT-IP", "community", ".1.3.6.1.4.1.3902.1012.3.50.12.1.1.14");
 
 $sql="INSERT INTO lokasi(`waktu_lokasi`,`lokasi`) VALUES(now(),?)";
 $sqlstat="INSERT INTO status(`waktu_status`,`status`) VALUES(now(),?)";
